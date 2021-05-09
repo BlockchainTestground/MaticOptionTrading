573b85d7dce468eb9306557f647649e502323c51
turupawn <ahmed.hn.43@gmail.com>
2021-05-08 21:23:12 -0600

    replaced frontend

---
+++ client/Readme.md
@@ -0,0 +1,15 @@
+Option trading platform for matic frontend. The smart contract backbone is located in this [this repo](https://github.com/Turupawn/OptionTradingFrontend).
+
+# Dependencies
+
+```bash
+npm install
+```
+
+# Run
+
+```bash
+lite-server
+```
+
+The server should be running now on port 3000.
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,121 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var contract;
+var accounts;
+var web3;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const displayOptions = async () => {
+  options_length = await contract.methods.getMaticOptsLength().call()
+  options_html = "<ul>"
+  for(i=0; i<options_length; i++)
+  {
+    option = await contract.methods.maticOpts(i).call()
+    options_html += "<li>"
+    options_html += "Amount: "
+    options_html += option.amount
+    options_html += " Buyer: "
+    options_html += option.buyer
+    options_html += " Writer "
+    options_html += option.writer
+    options_html += " Canceled: "
+    options_html += option.canceled
+    options_html += " Exercised: "
+    options_html += option.exercised
+    options_html += " Expiry: "
+    options_html += option.expiry
+    options_html += " Id: "
+    options_html += option.id
+    options_html += " Latest_cost: "
+    options_html += option.latestCost
+    options_html += " Premium: "
+    options_html += option.premium
+    options_html += " Strike: "
+    options_html += option.strike
+    options_html += "<button onclick='cancelOption("+ option.id +")'>Cancel</button>"
+    options_html += "<button onclick='buyOption("+ option.id +", "+ option.premium +")'>Buy</button>"
+    options_html += "<button onclick='exerciseOption("+ option.id +", "+ option.latestCost +")'>Exercise</button>"
+    options_html += "<button onclick='retrieveExpiredFunds("+ option.id +")'>Retrieve expired funds</button>"
+    options_html += "<button onclick='updateExerciseCost("+ option.id +")'>Update exercise cost</button>"
+    options_html += "</li>"
+  }
+  options_html += "</ul>"
+  $("#options").html(options_html);
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const cancelOption = async (option_id) => {
+  await contract.methods
+    .cancelOption(option_id)
+    .send({ from: accounts[0], gas: 400000 });
+  displayOptions(contract);
+}
+
+const buyOption = async (option_id, premium) => {
+  await contract.methods
+    .buyOption(option_id)
+    .send({ from: accounts[0], gas: 400000, value: premium });
+  displayOptions(contract);
+}
+
+const exerciseOption = async (option_id, latest_cost) => {
+  await contract.methods
+    .exercise(option_id)
+    .send({ from: accounts[0], gas: 400000, value: latest_cost });
+  displayOptions(contract);
+}
+
+const retrieveExpiredFunds = async (option_id) => {
+  await contract.methods
+    .retrieveExpiredFunds(option_id)
+    .send({ from: accounts[0], gas: 400000 });
+  displayOptions(contract);
+}
+
+const updateExerciseCost = async (option_id) => {
+  await contract.methods
+    .updateExerciseCost(option_id)
+    .send({ from: accounts[0], gas: 400000 });
+  displayOptions(contract);
+}
+
+const writeOption = (contract, accounts) => {
+  let strike;
+  let premium;
+  let expiry;
+  let tknAmt;
+  $("#strike").on("change", (e) => {
+    strike = e.target.value;
+  });
+  $("#premium").on("change", (e) => {
+    premium = e.target.value;
+  });
+  $("#expiry").on("change", (e) => {
+    expiry = e.target.value;
+  });
+  $("#tknAmt").on("change", (e) => {
+    tknAmt = e.target.value;
+  });
+  $("#write_option_form").on("submit", async (e) => {
+    e.preventDefault();
+    await contract.methods
+      .writeOption(strike, premium, expiry, tknAmt)
+      .send({ from: accounts[0], gas: 400000, value: tknAmt });
+      displayOptions(contract);
+  });
+};
+
+async function optionTradesApp() {
+  var awaitWeb3 = async function() {
+    web3 = await getWeb3();
+    var awaitContract = async function() {
+      contract = await getContract(web3)
+      var awaitAccounts = async function() {
+        accounts = await web3.eth.getAccounts()
+        writeOption(contract, accounts)
+        displayOptions(contract)
+      }
+      awaitAccounts()
+    }
+    awaitContract()
+  }
+  awaitWeb3()
+}
+
+optionTradesApp();
